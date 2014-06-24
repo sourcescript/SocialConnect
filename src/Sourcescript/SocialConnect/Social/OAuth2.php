e4440b0a06e8db3aa359a07071b8870eb708391a
rupaheizu <roop@hakz.co>
2014-06-24 11:44:17 +0800

    OCD cleanup

--- src/Sourcescript/SocialConnect/Social/OAuth2.php
+++ src/Sourcescript/SocialConnect/Social/OAuth2.php
@@ -1,13 +1,7 @@
 <?php namespace SourceScript\SocialConnect\Social;
 
-use \Config;
-use \Event;
-use \Input;
-use \Redirect;
-use \Session;
-
-
-use \Sourcescript\SocialConnect\URI\Chainable\Chainable;
+use Config, Event, Input, Redirect, Session;
+use Sourcescript\SocialConnect\URI\Chainable\Chainable;
 
 abstract class OAuth2 
 {
@@ -41,31 +35,30 @@ abstract class OAuth2
 					->setAttrib('client_secret', $config['app_secret'])
 					->setAttrib('scope',implode(',',$config['scopes']));
 			
-		if(!$code) {
+		if ( ! $code ) {
 			return $chainable->getURI();
-		}else {
+		} else {
 			return $chainable->setAttrib('code', $code)->getURI();
 		}
 	}
 
-	public  function getTokenUrl()
+	public function getTokenUrl()
 	{
 		return $this->payloadGraph."/oauth/access_token";
 	}	
 
-	public  function loginRedirect($redirect_uri = '')
+	public function loginRedirect($redirect_uri = '')
 	{
 		$class = get_called_class();
 		$login = $this->loginUrl($redirect_uri);
 		return Redirect::to($login);
 	}
 
-	public  function login($redirect_uri = '')
+	public function login($redirect_uri = '')
 	{
 		$config = Config::get('social-connect::config.'.$this->type);
 
-		if(!$this->hasAccessToken() 
-			&& !Input::get('code') ) {
+		if ( ! $this->hasAccessToken() && ! Input::get('code') ) {
 
 			return $this->loginRedirect($redirect_uri);
 
@@ -86,16 +79,16 @@ abstract class OAuth2
 
 	}
 
-	public  function hasAccessToken()
+	public function hasAccessToken()
 	{
 		return Session::get($this->type.'.access_token') ? true : false;
 	}
 
-	public  function getAccessToken($code = null, $redirect_uri = null)
+	public function getAccessToken($code = null, $redirect_uri = null)
 	{
 		$config = Config::get('social-connect::config.'.$this->type);
 
-		if(!$this->hasAccessToken()) {
+		if( ! $this->hasAccessToken() ) {
 
 			$chainable = Chainable::make($this->payloadGraph."/oauth/access_token");
 			$chainable->setAttrib('client_id', $config['app_id'])
@@ -111,18 +104,15 @@ abstract class OAuth2
 			$this->setAccessToken($contents[1]);
 
 			return $contents[1];
-		}else {
+		} else {
 			return Session::get($this->type.'.access_token');
 		}
 		
 	}
 
-	public  function setAccessToken($token)
+	public function setAccessToken($token)
 	{
-
 		return Session::put($this->type.'.access_token', $token);
 	}
-
-
 
 }
