a154c05ceafb8ef90a444b1e6a4b83c89a8f44d6
Kier Borromeo <seraphipod@gmail.com>
2014-06-24 16:42:20 +0800

    Fixed conflict on last pull request
    - Fixed a function being called as a property ( $this->hasAccessToken to $this->hasAccessToken() )
    - Refactored to start using injected instead hard-coded dependencies
    
    If I happen to overwrite anything from the last commit, I apologize.

--- src/Sourcescript/SocialConnect/Social/OAuth2.php
+++ src/Sourcescript/SocialConnect/Social/OAuth2.php
@@ -1,6 +1,10 @@
 <?php namespace SourceScript\SocialConnect\Social;
 
-use Config, Event, Input, Redirect, Session;
+use Illuminate\Config\Repository as Config;
+use Illuminate\Events\Dispatcher as Event;
+use Illuminate\Http\Request as Input;
+use Illuminate\Routing\Redirector as Redirect;
+use Illuminate\Session\Store as Session;
 use Sourcescript\SocialConnect\URI\Chainable\Chainable;
 
 abstract class OAuth2 
@@ -9,85 +13,149 @@ abstract class OAuth2
 	protected  $payloadGraph;
 	protected  $type;
 
+	/**
+	 * 
+	 * 
+	 * @param
+	 * @param
+	 * @param
+	 * @return
+	 */
 	public function __construct($type, $payloadLogin, $payloadGraph)
 	{
 		$this->type = $type;
 		$this->payloadLogin = $payloadLogin;
 		$this->payloadGraph = $payloadGraph;
+
+		// Inject dependencies
+		$this->setDependencies();
 	}
 
+	/**
+	 * Resolve class dependencies (setter injector)
+	 * 
+	 * @param Illuminate\Config\Repository $config
+	 * @param Illuminate\Events\Dispatcher $event
+	 * @param Illuminate\Http\Request $input
+	 * @param Illuminate\Routing\Redirector $redirect
+	 * @param Illuminate\Session\Store $session
+	 * @return void
+	 */
+	protected function setDependencies(
+		Config $config,
+		Event $event,
+		Input $input,
+		Redirect $redirect,
+		Session $session
+	)
+	{
+		$this->config = $config;
+		$this->event = $event;
+		$this->input = $input;
+		$this->redirect = $redirect;
+		$this->session = $session;
+	}
+
+	/**
+	 * Allows static creation of this class
+	 * 
+	 * @return {called class}
+	 */
 	public static function make()
 	{
 		$class = get_called_class();
 		return new $class;
 	}
 
-
+	/**
+	 * 
+	 * @param str	$redirect_uri
+	 * @param int	$code
+	 * @return str
+	 */
 	public function loginUrl($redirect_uri = '', $code = null)
 	{
 		$config = Config::get('social-connect::config.'.$this->type);
 
 		$chainable = Chainable::make($this->payloadLogin);
 		$chainable->setAttrib('client_id', $config['app_id'])
-			->setAttrib('redirect_uri', $redirect_uri)
-			->setAttrib('type', 'web_server')
-			->setAttrib('response_type', 'token')
-			->setAttrib('client_secret', $config['app_secret'])
-			->setAttrib('scope',implode(',',$config['scopes']));
+					->setAttrib('redirect_uri', $redirect_uri)
+					->setAttrib('type', 'web_server')
+					->setAttrib('response_type', 'token')
+					->setAttrib('client_secret', $config['app_secret'])
+					->setAttrib('scope',implode(',',$config['scopes']));
 
 		return ! $code ? $chainable->getURI() : $chainable->setAttrib('code', $code)->getURI();
 	}
 
-	public function getTokenUrl()
+	/**
+	 * 
+	 * @return str
+	 */
+	public  function getTokenUrl()
 	{
 		return $this->payloadGraph."/oauth/access_token";
 	}	
 
-	public function loginRedirect($redirect_uri = '')
+	/**
+	 *
+	 * @return Response
+	 */ 
+	public  function loginRedirect($redirect_uri = '')
 	{
 		$class = get_called_class();
 		$login = $this->loginUrl($redirect_uri);
-		return Redirect::to($login);
+		return $this->redirect->to($login);
 	}
 
 
 	// this function is fucked up, please fix it. huurrr
-	public function login($redirect_uri = '')
+	/**
+	 * 
+	 * @param str $redirect_uri
+	 * @return
+	 */
+	public  function login($redirect_uri = '')
 	{
-		$config = Config::get('social-connect::config.'.$this->type);
+		$config = $this->config->get('social-connect::config.'.$this->type);
 
-		if ( ! $this->hasAccessToken() && ! Input::get('code') ) {
+		if(!$this->hasAccessToken() && !$this->input->has('code') ) {
 
 			return $this->loginRedirect($redirect_uri);
 
-		}elseif(Input::get('error')) {
+		} elseif( $this->input->has('error') ) {
 
-			return Input::all();
+			return $this->input->all();
 
-		}elseif($code = Input::get('code')) {
+		} elseif($code = $this->input->get('code')) {
 
 			$this->getAccessToken($code, $redirect_uri);
 
 		}
 
-		if($this->hasAccessToken()) {
-			return Redirect::to($config['login']['redirect_uri']);
+		if( $this->hasAccessToken() ) {
+			return $this->redirect->to($config['login']['redirect_uri']);
 		}
+
 
 	}
 
-	public function hasAccessToken()
+	/**
+	 * 
+	 * @return bool
+	 */
+	public  function hasAccessToken()
 	{
-		return Session::get($this->type.'.access_token') ? true : false;
+		return $this->session->get($this->type.'.access_token') ? true : false;
 	}
 
 	public function getAccessToken($code = null, $redirect_uri = null)
 	{
-		if ( $this->hasAccessToken ) {
-			return Session::get($this->type . '.access_token');
+		if ( $this->hasAccessToken() ) {
+			return $this->session->get($this->type . '.access_token');
 		}
 
-		$config = Config::get('social-connect::config.'.$this->type);
+		$config = $this->config->get('social-connect::config.'.$this->type);
 
 		$chainable = Chainable::make($this->payloadGraph."/oauth/access_token");
 		$chainable->setAttrib('client_id', $config['app_id'])
@@ -105,9 +173,13 @@ abstract class OAuth2
 		return $contents[1];
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public function setAccessToken($token)
 	{
-		return Session::put($this->type.'.access_token', $token);
+		return $this->session->put($this->type.'.access_token', $token);
 	}
 
 }
